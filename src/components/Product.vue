7b1e100df43299a3fefca9356794c973d347afd1
Yunus Dermawan <yunus.shevcenko@gmail.com>
2023-03-22 23:08:16 +0700

    adding product component

---
+++ src/components/Product.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="product"
+    v-for="item in listItems"
+    :key="item.id"
+    :price="price"
+    >
+       <div class="product-inner">
+        <div class="product-text-wrap">
+            <h2 class="bg-text">{{item.title}}</h2>
+        </div>
+       </div>
+    </div>
+</template>
+<script>
+ export default {
+    data() {
+        return {
+            listItems: []
+        }
+    },
+    methods: {
+        async getData() {
+            const res = await fetch('https://fakestoreapi.com/products')
+            const finalRes = await res.json()
+            this.listItems = finalRes
+        }
+    },
+    mounted() {
+        this.getData()
+    }
+ }
+</script>
